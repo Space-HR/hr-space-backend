c758de39e3dddb2bccddd0c8b71df63264dbf4d5
edelveisx <edelveisx@gmail.com>
2024-03-20 16:44:21 +0500

    Изменено имя папки проекта

--- HRspace_backend/HRspace_backend/wsgi.py
+++ HRspace_backend/HRspace_backend/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HRspace.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HRspace_backend.settings')
 
 application = get_wsgi_application()
